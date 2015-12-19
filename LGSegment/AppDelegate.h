3c90ba1f9d1cc0453bbf18a1b7c5c78f4c8d718c
LiGo <ligoex@icloud.com>
2015-12-19 08:58:48 +0800

    Initial Commit

---
+++ LGSegment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LGSegment
+//
+//  Created by LiGo on 12/19/15.
+//  Copyright © 2015 LiGo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
